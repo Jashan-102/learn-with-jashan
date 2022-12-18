580a256d26c6efe1600e25d5ec7c56b3330d7cee
Jashandeep <jashandeepk22000@gmail.com>
2022-12-18 22:06:11 +0530

    'ok'

--- src/components/Learning/Learning.js
+++ src/components/Learning/Learning.js
@@ -6,6 +6,7 @@ export default function Learning() {
     <div className="container">
       <Link to="/learning/learn-js" className="me-4 pe-4">
         Learn JS
+        
       </Link>
       <Link to="/learning/learn-scss" className="me-4">
         Learn SCSS
